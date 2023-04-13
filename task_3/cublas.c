301d9cd1cceecf17826d89e7a133fdd242403162
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-04-13 21:47:39 +0700

    Update cublas.c

--- task_3/cublas.c
+++ task_3/cublas.c
@@ -1,3 +1,13 @@
+/*Данный код решает уравнение Пуассона на прямоугольной области с помощью метода Якоби и ускоряет вычисления с помощью директив OpenACC.
+Вначале определяются параметры модели, такие как точность, размер сетки и максимальное число итераций.
+Затем выделяется память на хосте и девайсе для двух массивов A и Anew,
+которые хранят значения температуры на сетке.
+
+Основной алгоритм выполняется до тех пор, пока значение переменной error не станет меньше заданного порога.
+На каждой итерации цикла происходит вычисление новых значений на текущей точке с использованием значений
+соседних точек и обновление значения error. Когда значение error становится меньше заданного порога,
+цикл прерывается и программа выводит результаты решения уравнения Пуассона на экран*/
+
 #include "/opt/nvidia/hpc_sdk/Linux_x86_64/21.11/math_libs/11.5/targets/x86_64-linux/include/cublas_v2.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,88 +15,106 @@
 #define n 1024
 
 int main() {
-    double *buf;
+    double *tmp;
     cublasHandle_t handle;
     cublasCreate(&handle);
 
     const double alpha = -1;
     double step1 = 10.0 / (n - 1);
 
-    double* u = (double*)calloc(n*n, sizeof(double));
-    double* up = (double*)calloc(n*n, sizeof(double));
+    double* A = (double*)calloc(n*n, sizeof(double));
+    double* A_new = (double*)calloc(n*n, sizeof(double));
     
     double x1 = 10.0, x2 = 20.0, y1 = 20.0, y2 = 30.0;
     
-    u[0] = up[0] = x1;
-    u[n] = up[n] = x2;
-    u[n * (n - 1) + 1] = up[n * (n - 1) + 1] = y1;
-    u[n * n] = up[n * n] = y2;
+    //Заполнение угловых значений сетки
+    A[0] = A_new[0] = x1;
+    A[n] = A_new[n] = x2;
+    A[n * (n - 1) + 1] = A_new[n * (n - 1) + 1] = y1;
+    A[n * n] = A_new[n * n] = y2;
 
-#pragma acc enter data create(u[0:n*n], up[0:n*n]) copyin(n, step1)
+//Создание копии массивов A и A_new на устройстве, а также копирование значений n и step1
+#pragma acc enter data create(A[0:n*n], A_new[0:n*n]) copyin(n, step1)
+//Указываем на начало параллельной области кода
 #pragma acc kernels
     {
 #pragma acc loop independent
+        //Инициализация граничных значений массива A и A_new,
+        //которые соответствуют граничным условиям задачи решения уравнения Пуассона
         for (int i = 0; i < n; i++) {
-            u[i*n] = up[i*n] = x1 + i * step1;
-            u[i] = up[i] = x1 + i * step1;
-            u[(n - 1) * n + i] = up[(n - 1) * n + i] = y1 + i * step1;
-            u[i * n + (n - 1)] = up[i * n + (n - 1)] = x2 + i * step1;        }
+            A[i*n] = A_new[i*n] = x1 + i * step1;
+            A[i] = A_new[i] = x1 + i * step1;
+            A[(n - 1) * n + i] = A_new[(n - 1) * n + i] = y1 + i * step1;
+            A[i * n + (n - 1)] = A_new[i * n + (n - 1)] = x2 + i * step1;        }
     }
 
-    int itter = 0;
+    int iter = 0;
     double error = 1.0;
     {
-    while (itter < 1000000 && error > 1e-6) {
-        itter++;
-        if (itter % 100 == 0 || itter == 1) {
-#pragma acc data present(u[0:n*n], up[0:n*n])
+    while (iter < 1000000 && error > 1e-6) {
+        iter++;
+        if (iter % 100 == 0 || iter == 1) {
+//Создаём копии массивов A и A_new на устройстве
+#pragma acc data present(A[0:n*n], A_new[0:n*n])
+//Начало параллельной области с указанием асинхронности выполнения
 #pragma acc kernels async(1)
             {
+//Распараллеливанием циклы по итерациям и потокам
 #pragma acc loop independent collapse(2)
                 for (int i = 1; i < n - 1; i++) {
                     for (int j = 1; j < n - 1; j++) {
-                        up[i * n + j] =
-                                0.25 * (u[(i + 1) * n + j] + u[(i - 1) * n + j] + u[i * n + j - 1] + u[i * n + j + 1]);
+                        A_new[i * n + j] =
+                                0.25 * (A[(i + 1) * n + j] + A[(i - 1) * n + j] + A[i * n + j - 1] + A[i * n + j + 1]);
                     }
                 }
             }
             int id = 0;
+//Ожидание завершения асинхронных операций
 #pragma acc wait
-#pragma acc host_data use_device(u, up)
+//Копируем данные с устройства на хост
+#pragma acc host_data use_device(A, A_new)
             {
-                cublasDaxpy(handle, n * n, &alpha, up, 1, u, 1);
-                cublasIdamax(handle, n * n, u, 1, &id);
+                cublasDaxpy(handle, n * n, &alpha, A_new, 1, A, 1);
+                cublasIdamax(handle, n * n, A, 1, &id);
 
             }
+//Копируем значения элемента с максимальным значением на хост
 #pragma acc update self(u[id-1:1])
-            error = fabs(u[id - 1]);
-#pragma acc host_data use_device(u, up)
-            cublasDcopy(handle, n * n, up, 1, u, 1);
+            error = fabs(A[id - 1]);
+//Копируем данные с хоста на устройство
+#pragma acc host_data use_device(A, A_new)
+            cublasDcopy(handle, n * n, A_new, 1, A, 1);
 
         } else {
-#pragma acc data present(u[0:n*n], up[0:n*n])
+//Создаём копии массивов A и A_new на устройстве
+#pragma acc data present(A[0:n*n], A_new[0:n*n])
+//Указываем начало параллельной области с указанием асинхронности выполнения
 #pragma acc kernels async(1)
             {
+//Распараллеливаем циклы по итерациям и потокам
 #pragma acc loop independent collapse(2)
                 for (int i = 1; i < n - 1; i++) {
                     for (int j = 1; j < n - 1; j++) {
-                        up[i * n + j] =
-                                0.25 * (u[(i + 1) * n + j] + u[(i - 1) * n + j] + u[i * n + j - 1] + u[i * n + j + 1]);
+                        A_new[i * n + j] =
+                                0.25 * (A_new[(i + 1) * n + j] + A_new[(i - 1) * n + j] + A_new[i * n + j - 1] + A_new[i * n + j + 1]);
                     }
                 }
             }
         }
-        buf = u;
-        u = up;
-        up = buf;
-
-        if (itter % 100 == 0 || itter == 1)
+        tmp = A;
+        A = A_new;
+        A_new = tmp;
+        
+        //Отслеживаем прогресс вычислений
+        if (iter % 100 == 0 || iter == 1)
 #pragma acc wait(1)
-            printf("%d %e\n", itter, error);
+            printf("%d %e\n", iter, error);
 
     }
 }
-    printf("%d\n", itter);
+    
+    //Вывод результатов
+    printf("%d\n", iter);
     printf("%e", error);
     cublasDestroy(handle);
     return 0;
